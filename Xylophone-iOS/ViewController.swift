5ddf267959ba50b76ec9a8a9e339b401f4f89964
Min Pyae <minmp2002@gmail.com>
2024-01-23 13:38:09 +0000

    Completed ViewController

--- Xylophone-iOS/ViewController.swift
+++ Xylophone-iOS/ViewController.swift
@@ -6,17 +6,42 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
     @IBAction func keyButtonOnPressed(_ sender: UIButton) {
+        if let keyTitle = sender.currentTitle {
+            playSound(soundName: keyTitle)
+        }
         
+        // REDUCING BUTTON OPACITY TO HALF
+        sender.alpha = 0.5
+        
+        // EXECUTING AFTER 0.1 SECONDS DELAY
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+            
+            // BRINGING BUTTON'S OPACITY BACK TO FULLY OPAQUE
+            sender.alpha = 1.0
+            
+        }
     }
     
+    func playSound(soundName: String) {
+        
+        if let url = Bundle.main.url(forResource: soundName, withExtension: "wav") {
+            player = try! AVAudioPlayer(contentsOf: url)
+            player.play()
+        }
+        
+    }
+
 }
 
